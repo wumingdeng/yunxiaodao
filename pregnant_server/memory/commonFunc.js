6084da72bde4039b4981935b56392656f0d1e654
Fizzo <809050631@qq.com>
2017-06-14 20:16:50 +0800

    后台的推送体重管理的配置信息

--- pregnant_server/memory/commonFunc.js
+++ pregnant_server/memory/commonFunc.js
@@ -2,60 +2,72 @@
 var m = require('./memoryVarible')
 var db = require('../models')
 var mod = {
-    ReloadMemory :function(tbl){
+    ReloadMemory: function (tbl) {
         // reload all
         // configs,swipe_configs,services,catalogs,work_services
-        db[tbl].findAll().then(function(data){
+        db[tbl].findAll().then(function (data) {
             m[tbl] = {}
-            for(var i = 0;i<data.length;i++){
-                m[tbl][data[i].id]=data[i].dataValues
+            for (var i = 0; i < data.length; i++) {
+                m[tbl][data[i].id] = data[i].dataValues
             }
         })
     },
-    ReloadWorkersMemory :function(id){
+    ReloadWorkersMemory: function (id) {
         // reload single
-        db.workers.findAll({where:{id:id}}).then(function(data){
-            for(var i = 0;i<data.length;i++){
+        db.workers.findAll({ where: { id: id } }).then(function (data) {
+            for (var i = 0; i < data.length; i++) {
                 m.workers[data[i].id] = data[i].dataValues
             }
         })
     },
-    ReloadCatServicesMemory :function(){
-        db.cat_services.findAll().then(function(data6){
+    ReloadCatServicesMemory: function () {
+        db.cat_services.findAll().then(function (data6) {
             m.cat_services = {}
-            for(var i = 0;i<data6.length;i++){
-                if(m.cat_services[data6[i].cat_id]){
+            for (var i = 0; i < data6.length; i++) {
+                if (m.cat_services[data6[i].cat_id]) {
                     m.cat_services[data6[i].cat_id].push(data6[i].service_id)
-                }else{
-                    m.cat_services[data6[i].cat_id]=[data6[i].service_id]
+                } else {
+                    m.cat_services[data6[i].cat_id] = [data6[i].service_id]
                 }
             }
         })
     },
-    InitDbMemory :function(){
-        db.swipe_configs.findAll().then(function(data2){
-            for(var i = 0;i<data2.length;i++){
-                m.swipe_configs[data2[i].id]=data2[i].dataValues
+    InitDbMemory: function () {
+        db.swipe_configs.findAll().then(function (data2) {
+            for (var i = 0; i < data2.length; i++) {
+                m.swipe_configs[data2[i].id] = data2[i].dataValues
             }
-            db.products.findAll().then(function(data3){
-                for(var i = 0;i<data3.length;i++){
-                    m.products[data3[i].id]=data3[i].dataValues
+            db.products.findAll().then(function (data3) {
+                for (var i = 0; i < data3.length; i++) {
+                    m.products[data3[i].id] = data3[i].dataValues
                 }
-                db.weightAdvice_configs.findAll().then(function(data4){
-                    for(var i = 0;i<data4.length;i++){
-                        m.weightAdvice_configs[data4[i].id]=data4[i].dataValues
+                db.weightAdvice_configs.findAll().then(function (data4) {
+                    for (var i = 0; i < data4.length; i++) {
+                        m.weightAdvice_configs[data4[i].id] = data4[i].dataValues
                     }
-                    db.weightRate_configs.findAll().then(function(data5){
-                        for(var i = 0;i<data5.length;i++){
-                            m.weightRate_configs[data5[i].id]=data5[i].dataValues
+                    db.weightRate_configs.findAll().then(function (data5) {
+                        for (var i = 0; i < data5.length; i++) {
+                            m.weightRate_configs[data5[i].id] = data5[i].dataValues
                         }
-                        db.weight_diet_configs.findAll().then(function(data6) {
-                            for(var i = 0;i<data6.length;i++){
+                        db.weight_diet_configs.findAll().then(function (data6) {
+                            for (var i = 0; i < data6.length; i++) {
                                 m.weight_diet_configs[data6[i].week] = data6[i].dataValues
                             }
                         })
                     })
                 })
+            })
+        })
+    },
+    reloadWeightConfig: function () {
+        db.weightAdvice_configs.findAll().then(function (data4) {
+            for (var i = 0; i < data4.length; i++) {
+                m.weightAdvice_configs[data4[i].id] = data4[i].dataValues
+            }
+            db.weight_diet_configs.findAll().then(function (data6) {
+                for (var i = 0; i < data6.length; i++) {
+                    m.weight_diet_configs[data6[i].week] = data6[i].dataValues
+                }
             })
         })
     }
