b3e9520d91438362c07328a6766d276db88f18b7
Fizzo <809050631@qq.com>
2017-06-19 20:02:33 +0800

    调整标题

--- pregnant/src/components/title.vue
+++ pregnant/src/components/title.vue
@@ -2,7 +2,7 @@
     <section label="Copyright ? 2017 playhudong All Rights Reserved." style="border: none; width: 100%; margin: 0.5em auto; text-align: left;" id="shifu_lin_013" donone="shifuMouseDownPayStyle(&#39;shifu_lin_013&#39;)">
         <section style="display:inline-block;vertical-align: middle;padding:0 3px 0 0">
             <section style="border-top:3px solid transparent;border-left:3px solid #666;float:right;"></section>
-            <section class="xhr" style="background: rgb(254, 67, 101); transform: rotate(360deg); color: rgb(255, 255, 255); font-size: 1em; margin-right: 3px; padding: 0.5em;">
+            <section class="xhr" style="background: rgb(251,113,146); transform: rotate(360deg); color: rgb(255, 255, 255); font-size: 1em; margin-right: 3px; padding: 0.5em;">
                 <p style="margin:0">
                     <br/>
                 </p>
