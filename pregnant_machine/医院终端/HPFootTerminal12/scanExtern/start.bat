bcd86935065196ed826e3f46e35923c3ff22f704
DESKTOP-VBSSR0N\fizzo <kaellzt777@gmail.com>
2017-06-29 17:26:58 +0800

    image change

--- pregnant_machine/医院终端/HPFootTerminal12/scanExtern/start.bat
+++ pregnant_machine/医院终端/HPFootTerminal12/scanExtern/start.bat
@@ -2,4 +2,6 @@ cd %1
 ::start TwainScanner.exe left
 ::TwainScanner.exe right
 
-convert -size 2000x1170 -strip xc:none .\Image\Image_Left.jpg -geometry +0+0 -composite .\Image\Image_Right.jpg -geometry +1150+0 -composite .\Image\fin.jpg
+convert .\Image\Image_Right.jpg -rotate -180 .\Image\Image_Right_r.jpg
+convert -size 2000x1170 -strip xc:none .\Image\Image_Left.jpg -geometry +0+0 -composite .\Image\Image_Right_r.jpg -geometry +1150+0 -composite .\Image\fin.jpg
+::convert -size 2000x1170 -strip xc:none .\Image\Image_Left.jpg -geometry +0+0 -composite .\Image\Image_Right.jpg -geometry +1150+0 -composite .\Image\fin.jpg
