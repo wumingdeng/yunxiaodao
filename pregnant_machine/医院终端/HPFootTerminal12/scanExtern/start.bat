a6bc8c785c2deab85c6510d9b39d5c5897d95125
DESKTOP-VBSSR0N\fizzo <kaellzt777@gmail.com>
2017-07-07 17:52:16 +0800

    a-4 fix

--- pregnant_machine/医院终端/HPFootTerminal12/scanExtern/start.bat
+++ pregnant_machine/医院终端/HPFootTerminal12/scanExtern/start.bat
@@ -3,5 +3,6 @@ cd %1
 ::TwainScanner.exe right
 
 convert .\Image\Image_Right.jpg -rotate -180 .\Image\Image_Right_r.jpg
-convert -size 2000x1170 -strip xc:none .\Image\Image_Left.jpg -geometry +0+0 -composite .\Image\Image_Right_r.jpg -geometry +1150+0 -composite .\Image\fin.jpg
+convert -size 2000x1370 -strip xc:none .\Image\Image_Left.jpg -geometry +0+100 -composite .\Image\Image_Right_r.jpg -geometry +1150+100 -composite .\Image\fin.jpg
+::convert -size 2000x1170 -strip xc:none .\Image\Image_Left.jpg -geometry +0+0 -composite .\Image\Image_Right_r.jpg -geometry +1150+0 -composite .\Image\fin.jpg
 ::convert -size 2000x1170 -strip xc:none .\Image\Image_Left.jpg -geometry +0+0 -composite .\Image\Image_Right.jpg -geometry +1150+0 -composite .\Image\fin.jpg
