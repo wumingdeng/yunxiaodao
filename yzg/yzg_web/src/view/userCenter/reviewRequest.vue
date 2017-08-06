afa2a5196b392817a3df297f80d3557a4d77be35
Fizzo <809050631@qq.com>
2017-08-06 16:01:40 +0800

    审核推广人

--- yzg/yzg_web/src/view/userCenter/reviewRequest.vue
+++ yzg/yzg_web/src/view/userCenter/reviewRequest.vue
@@ -1,147 +1,193 @@
 <template>
 	<f7-page navbar-through>
-	  <f7-navbar sliding>
-      <f7-nav-left>
-        <f7-link icon="icon-back color-black" @click="$router.go(-1)"></f7-link>
-      </f7-nav-left>
-      <f7-nav-center sliding style="left:-22px;" title=""></f7-nav-center>
-      <f7-nav-right></f7-nav-right>
-	  </f7-navbar>
-	  <f7-block inner>
-		  <f7-list id="userinfoForm" form style='margin-top:0px;'>
-	      <f7-list-item>
-	        <f7-label>推荐人</f7-label>
-	        <f7-input id="nameInput" class="fillInput" type="text" readonly v-model="$store.state.currentRequest.upName"></f7-input>
-	        <f7-button style="float:right; width:30%;" @click="onCheck">详情</f7-button>
-	      </f7-list-item>
-	      <f7-list-item>
-	        <f7-label>姓名</f7-label>
-	        <f7-input id="nameInput" class="fillInput" type="text" readonly v-model="$store.state.currentRequest.realName"></f7-input>
-	      </f7-list-item>
-	      <f7-list-item>
-	        <f7-label>手机号码</f7-label>
-	        <div class="item-input custom">
-	          <input id="phoneInput" type="number" readonly v-model="$store.state.currentRequest.phone">
-	        </div>
-	      </f7-list-item>
-	      <f7-list-item>
-	        <f7-label>职业</f7-label>
-	        <f7-input id="jobInput" type="text" readonly v-model="job"></f7-input>
-	      </f7-list-item>
-	      <f7-list-item>
-	        <f7-label>执业医院</f7-label>
-	        <f7-input id="hospitalInput" type="text" readonly v-model="$store.state.currentRequest.hospital"></f7-input>
-	      </f7-list-item>
-	      <f7-list-item>
-	        <f7-label>科室</f7-label>
-	        <f7-input id="departmentInput" type="text" readonly v-model="$store.state.currentRequest.department"></f7-input>
-	      </f7-list-item>
-	      <f7-list-item>
-	        <f7-label>审核建议</f7-label>
-	        <f7-input id="adviceInput" type="textarea"></f7-input>
-	      </f7-list-item>
-	    </f7-list>
-	    <img :src="cert" style="width:100%;">
-			<f7-grid>
+		<f7-navbar sliding>
+			<f7-nav-left>
+				<f7-link icon="icon-back color-black" @click="$router.go(-1)"></f7-link>
+			</f7-nav-left>
+			<f7-nav-center sliding style="left:-22px;" title=""></f7-nav-center>
+			<f7-nav-right></f7-nav-right>
+		</f7-navbar>
+		<f7-card style='font-size:18px;border-radius:7px'>
+			<f7-card-content>
+				<spane>推荐人</spane>
+				<spane>&nbsp;&nbsp;&nbsp;{{$store.state.currentRequest.upName}}</spane>
+				<f7-button fill style="float:right; width:30%;background:#68cfc1;font-size:18px" @click="onCheck">详情</f7-button>
+			</f7-card-content>
+		</f7-card>
+		<f7-card inner style='border-radius:7px;margin: 10px;padding:20px 0px 5px 0px;'>
+			<f7-list id="userinfoForm" form style='margin-top:0px'>
+				<f7-list-item :title="'姓名'" class="cus_item">
+					<f7-label class='cus_label'>{{$store.state.currentRequest.realName}}</f7-label>
+				</f7-list-item>
+				<f7-list-item :title="'手机号码'" class="cus_item">
+					<f7-label class='cus_label'>{{$store.state.currentRequest.phone}}</f7-label>
+				</f7-list-item>
+				<f7-list-item :title="'职业'" class="cus_item">
+					<f7-label class='cus_label'>{{job}}</f7-label>
+				</f7-list-item>
+				<f7-list-item :title="'执业医院'" class="cus_item">
+					<f7-label class='cus_label'>{{$store.state.currentRequest.hospital}}</f7-label>
+				</f7-list-item>
+				<f7-list-item :title="'科室'" class="cus_item">
+					<f7-label class='cus_label'>{{$store.state.currentRequest.department}}</f7-label>
+				</f7-list-item>
+				<f7-list-item class="cus_item">
+					<f7-label style='width:30%;margin-bottom:100px'>执业证书</f7-label>
+					<v-touch
+            tag='img' 
+            style="width:30%; display:block;" 
+            v-lazy="cert"
+            v-bind:enabled="{ swipe: false}"
+            @tap="openPhotoBrowser(1)"
+            @swipeup="onTouchStart"
+            >
+          </v-touch>
+          <f7-photo-browser
+            ref="pb"
+            :photos="photos"
+            backLinkText=""
+            ofText="/"
+            :loop="true"
+            :exposition="false"
+            :expositionHideCaptions="false"
+            :lazyLoading="true"
+            :lazyLoadingInPrevNext="true"
+            @click="onClickPB"
+          ></f7-photo-browser>
+				</f7-list-item>
+				<f7-list-item>
+					<f7-label style='width:30%;margin-bottom:70px'>审核建议</f7-label>
+					<f7-input id="adviceInput" style='background:rgb(230,230,230;border-radius:20px;margin:10px 0' readonly type="textarea"></f7-input>
+				</f7-list-item>
+			</f7-list>
+			<f7-grid style='margin:10px'>
 				<f7-col width=50>
-					<f7-button fill big color="red" @click="onRefuse">审核不通过</f7-button>
+					<f7-button fill big style='background:#68cfc1' @click="onAccept">审核通过</f7-button>
 				</f7-col>
 				<f7-col width=50>
-					<f7-button fill big color="green" @click="onAccept">审核通过</f7-button>
+					<f7-button fill big style='background:rgba(0,0,0,0);color:#68cfc1;border:2px solid #68cfc1' @click="onRefuse">审核不通过</f7-button>
 				</f7-col>
 			</f7-grid>
-		</f7-block>
+		</f7-card>
 	</f7-page>
 </template>
 
 <script>
-	import g from '../../globals/global'
-	export default {
-		data() {
-			return {
-				upid: null,	//上线
-				isTouch: false, //连点控制
-				isRequest:false 	//是否为申请推广人
-			}
-		},
-		computed:{
-			job() {
-				return this.$store.state.currentRequest.job == 1 ? '医生': '护士';
-			},
-			cert() {
+import g from '../../globals/global'
+export default {
+	data() {
+		return {
+			upid: null,	//上线
+			isTouch: false, //连点控制
+			isRequest: false 	//是否为申请推广人
+		}
+	},
+	computed: {
+		photos() {
 				var cert = this.$store.state.currentRequest.certificate;
-				if (cert) {
-					return g.serverAddress + '/' + cert;
-				} else {
-					return ''
-				}
-			}
+				return [g.serverAddress + '/'+cert]
+				// return [g.serverAddress + '/certificate/cert-oni2YwgQMsC6sEFHTde3xA5aH0_A.jpeg']
+      },
+		job() {
+			return this.$store.state.currentRequest.job == 1 ? '医生' : '护士';
 		},
-		methods:{
-			onCheck() {
-				this.$router.push({
-					path: '/checkSaleman',
-					query:{
-						userid:this.$store.state.currentRequest.upid
-					}
-				})
-			},
-			onAccept() {
-				var self = this
-				this.$f7.confirm('您将通过申请',function() {
-					self.$store.dispatch('acceptRequest',{
-						self: self,
-						info:{
-							id: self.$store.state.currentRequest.id
-						},
-						callback(self, res){
-							if (res.body.ok == 0) {
-								self.$f7.alert('','已经通过推广申请',function() {
-									self.$router.go(-1);
-								})
-							}
-						}
-					})
-				})
-			},
-			onRefuse(){
-				var adviceInput = document.getElementById('adviceInput').value
-				if (!adviceInput) {
-					this.$f7.alert('','请填写审核建议');
-					return false
+		cert() {
+			var cert = this.$store.state.currentRequest.certificate;
+			console.log(cert)
+			if (cert) {
+				// return g.serverAddress + '/certificate/cert-oni2YwgQMsC6sEFHTde3xA5aH0_A.jpeg';
+				return g.serverAddress + '/'+cert;
+			} else {
+				return ''
+			}
+		}
+	},
+	methods: {
+		onTouchStart() {
+        console.log('touchstart')
+      },
+		openPhotoBrowser(index) {
+        console.log(index)
+        this.$refs.pb.open(index)
+        this.$refs.pb.enableExposition()
+      },
+      onClickPB(swiper,event) {
+        this.$nextTick(this.$refs.pb.close)
+      },
+		onCheck() {
+			this.$router.push({
+				path: '/checkSaleman',
+				query: {
+					userid: this.$store.state.currentRequest.upid
 				}
-				this.$store.dispatch('refuseRequest',{
-					self: this,
-					info:{
-						id: this.$store.state.currentRequest.id,
-						advice: adviceInput
+			})
+		},
+		onAccept() {
+			var self = this
+			this.$f7.confirm('您将通过申请', function () {
+				self.$store.dispatch('acceptRequest', {
+					self: self,
+					info: {
+						id: self.$store.state.currentRequest.id
 					},
 					callback(self, res) {
 						if (res.body.ok == 0) {
-							self.$f7.alert('','已经拒绝此推广申请',function() {
+							self.$f7.alert('', '已经通过推广申请', function () {
 								self.$router.go(-1);
 							})
 						}
 					}
 				})
-			}
+			})
 		},
-		mounted() {
-			this.isRequest = this.$route.query.request;
-			this.upid = this.$route.query.upid;
+		onRefuse() {
+			var adviceInput = document.getElementById('adviceInput').value
+			if (!adviceInput) {
+				this.$f7.alert('', '请填写审核建议');
+				return false
+			}
+			this.$store.dispatch('refuseRequest', {
+				self: this,
+				info: {
+					id: this.$store.state.currentRequest.id,
+					advice: adviceInput
+				},
+				callback(self, res) {
+					if (res.body.ok == 0) {
+						self.$f7.alert('', '已经拒绝此推广申请', function () {
+							self.$router.go(-1);
+						})
+					}
+				}
+			})
 		}
-	}
+	},
+	mounted() {
+		this.isRequest = this.$route.query.request;
+		this.upid = this.$route.query.upid;
+	},
+	beforeDestroy() {
+      this.$refs.pb.close();
+      this.$f7.closeModal()
+    }
+}
 </script>
 
-<style scoped>
-/*	.fillInput {
-		background-color:#eeeeee;
-		margin:5px;
-		border-radius:5px;
-	}
-	.fillInput input {
-		text-align:center;
-	}*/
+<style>
+.content-block-inner {
+	border-radius: 10px;
+}
+.cus_item {
+	border-bottom:1px solid rgb(196,195,200,0.7)
+}
+.cus_item .item-inner:after {
+		background-color: rgba(0,0,0,0);
+}
+
+.cus_label {
+	text-align: right;
+	color:rgb(150,150,150)
+}
 </style>
 
 
